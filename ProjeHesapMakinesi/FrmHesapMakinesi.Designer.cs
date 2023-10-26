718e5331e5f104390c88df89355c969a926ba591
Soner Yeşilay <soneryesilay@outlook.com>
2023-10-26 14:08:03 +0300

    Add project files.

---
+++ ProjeHesapMakinesi/FrmHesapMakinesi.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace ProjeHesapMakinesi
+{
+    partial class FrmHesapMakinesi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmHesapMakinesi));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.BtnVirgul = new System.Windows.Forms.Button();
+            this.BtnTemizle = new System.Windows.Forms.Button();
+            this.BtnHesapla = new System.Windows.Forms.Button();
+            this.BtnTopla = new System.Windows.Forms.Button();
+            this.BtnCıkar = new System.Windows.Forms.Button();
+            this.BtnCarp = new System.Windows.Forms.Button();
+            this.BtnBol = new System.Windows.Forms.Button();
+            this.TxtDisplay = new System.Windows.Forms.TextBox();
+            this.BtnBackSpace = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(17, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 64);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.SystemColors.Window;
+            this.button2.Location = new System.Drawing.Point(140, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 64);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.SystemColors.Window;
+            this.button3.Location = new System.Drawing.Point(263, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 64);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button4.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button4.ForeColor = System.Drawing.SystemColors.Window;
+            this.button4.Location = new System.Drawing.Point(17, 244);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 64);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button5.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button5.ForeColor = System.Drawing.SystemColors.Window;
+            this.button5.Location = new System.Drawing.Point(140, 244);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(117, 64);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button6.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button6.ForeColor = System.Drawing.SystemColors.Window;
+            this.button6.Location = new System.Drawing.Point(263, 244);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(117, 64);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button7.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button7.ForeColor = System.Drawing.SystemColors.Window;
+            this.button7.Location = new System.Drawing.Point(17, 314);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(117, 64);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button8.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button8.ForeColor = System.Drawing.SystemColors.Window;
+            this.button8.Location = new System.Drawing.Point(140, 314);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(117, 64);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button9.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button9.ForeColor = System.Drawing.SystemColors.Window;
+            this.button9.Location = new System.Drawing.Point(263, 314);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(117, 64);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // button0
+            // 
+            this.button0.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button0.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.button0.ForeColor = System.Drawing.SystemColors.Window;
+            this.button0.Location = new System.Drawing.Point(17, 384);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(117, 64);
+            this.button0.TabIndex = 15;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = false;
+            this.button0.Click += new System.EventHandler(this.Butonlar);
+            // 
+            // BtnVirgul
+            // 
+            this.BtnVirgul.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnVirgul.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.BtnVirgul.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnVirgul.Location = new System.Drawing.Point(140, 384);
+            this.BtnVirgul.Name = "BtnVirgul";
+            this.BtnVirgul.Size = new System.Drawing.Size(117, 64);
+            this.BtnVirgul.TabIndex = 16;
+            this.BtnVirgul.Text = ",";
+            this.BtnVirgul.UseVisualStyleBackColor = false;
+            this.BtnVirgul.Click += new System.EventHandler(this.BtnVirgul_Click);
+            // 
+            // BtnTemizle
+            // 
+            this.BtnTemizle.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnTemizle.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnTemizle.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnTemizle.Location = new System.Drawing.Point(263, 384);
+            this.BtnTemizle.Name = "BtnTemizle";
+            this.BtnTemizle.Size = new System.Drawing.Size(117, 64);
+            this.BtnTemizle.TabIndex = 17;
+            this.BtnTemizle.Text = "C";
+            this.BtnTemizle.UseVisualStyleBackColor = false;
+            this.BtnTemizle.Click += new System.EventHandler(this.BtnTemizle_Click);
+            // 
+            // BtnHesapla
+            // 
+            this.BtnHesapla.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.BtnHesapla.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.BtnHesapla.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnHesapla.Location = new System.Drawing.Point(140, 454);
+            this.BtnHesapla.Name = "BtnHesapla";
+            this.BtnHesapla.Size = new System.Drawing.Size(117, 64);
+            this.BtnHesapla.TabIndex = 19;
+            this.BtnHesapla.Text = "=";
+            this.BtnHesapla.UseVisualStyleBackColor = false;
+            this.BtnHesapla.Click += new System.EventHandler(this.BtnHesapla_Click);
+            // 
+            // BtnTopla
+            // 
+            this.BtnTopla.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnTopla.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.BtnTopla.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnTopla.Location = new System.Drawing.Point(48, 122);
+            this.BtnTopla.Name = "BtnTopla";
+            this.BtnTopla.Size = new System.Drawing.Size(73, 46);
+            this.BtnTopla.TabIndex = 2;
+            this.BtnTopla.Text = "+";
+            this.BtnTopla.UseVisualStyleBackColor = false;
+            this.BtnTopla.Click += new System.EventHandler(this.BtnTopla_Click);
+            // 
+            // BtnCıkar
+            // 
+            this.BtnCıkar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnCıkar.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.BtnCıkar.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnCıkar.Location = new System.Drawing.Point(127, 122);
+            this.BtnCıkar.Name = "BtnCıkar";
+            this.BtnCıkar.Size = new System.Drawing.Size(73, 46);
+            this.BtnCıkar.TabIndex = 3;
+            this.BtnCıkar.Text = "-";
+            this.BtnCıkar.UseVisualStyleBackColor = false;
+            this.BtnCıkar.Click += new System.EventHandler(this.BtnCıkar_Click);
+            // 
+            // BtnCarp
+            // 
+            this.BtnCarp.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnCarp.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.BtnCarp.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnCarp.Location = new System.Drawing.Point(206, 122);
+            this.BtnCarp.Name = "BtnCarp";
+            this.BtnCarp.Size = new System.Drawing.Size(73, 46);
+            this.BtnCarp.TabIndex = 4;
+            this.BtnCarp.Text = "x";
+            this.BtnCarp.UseVisualStyleBackColor = false;
+            this.BtnCarp.Click += new System.EventHandler(this.BtnCarp_Click);
+            // 
+            // BtnBol
+            // 
+            this.BtnBol.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnBol.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.BtnBol.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnBol.Location = new System.Drawing.Point(285, 122);
+            this.BtnBol.Name = "BtnBol";
+            this.BtnBol.Size = new System.Drawing.Size(73, 46);
+            this.BtnBol.TabIndex = 5;
+            this.BtnBol.Text = "/";
+            this.BtnBol.UseVisualStyleBackColor = false;
+            this.BtnBol.Click += new System.EventHandler(this.BtnBol_Click);
+            // 
+            // TxtDisplay
+            // 
+            this.TxtDisplay.BackColor = System.Drawing.SystemColors.Window;
+            this.TxtDisplay.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold);
+            this.TxtDisplay.Location = new System.Drawing.Point(23, 44);
+            this.TxtDisplay.Name = "TxtDisplay";
+            this.TxtDisplay.Size = new System.Drawing.Size(351, 38);
+            this.TxtDisplay.TabIndex = 1;
+            this.TxtDisplay.Text = "0";
+            this.TxtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // BtnBackSpace
+            // 
+            this.BtnBackSpace.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BtnBackSpace.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnBackSpace.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnBackSpace.Location = new System.Drawing.Point(263, 454);
+            this.BtnBackSpace.Name = "BtnBackSpace";
+            this.BtnBackSpace.Size = new System.Drawing.Size(117, 64);
+            this.BtnBackSpace.TabIndex = 20;
+            this.BtnBackSpace.Text = "<=";
+            this.BtnBackSpace.UseVisualStyleBackColor = false;
+            this.BtnBackSpace.Click += new System.EventHandler(this.BtnBackSpace_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button10.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.button10.ForeColor = System.Drawing.SystemColors.Window;
+            this.button10.Location = new System.Drawing.Point(17, 454);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(117, 64);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "+/-";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // FrmHesapMakinesi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(397, 534);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.BtnBackSpace);
+            this.Controls.Add(this.TxtDisplay);
+            this.Controls.Add(this.BtnBol);
+            this.Controls.Add(this.BtnCarp);
+            this.Controls.Add(this.BtnCıkar);
+            this.Controls.Add(this.BtnTopla);
+            this.Controls.Add(this.BtnHesapla);
+            this.Controls.Add(this.BtnTemizle);
+            this.Controls.Add(this.BtnVirgul);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmHesapMakinesi";
+            this.Text = "Hesap Makinesi";
+            this.Load += new System.EventHandler(this.FrmHesapMakinesi_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button BtnVirgul;
+        private System.Windows.Forms.Button BtnTemizle;
+        private System.Windows.Forms.Button BtnHesapla;
+        private System.Windows.Forms.Button BtnTopla;
+        private System.Windows.Forms.Button BtnCıkar;
+        private System.Windows.Forms.Button BtnCarp;
+        private System.Windows.Forms.Button BtnBol;
+        private System.Windows.Forms.TextBox TxtDisplay;
+        private System.Windows.Forms.Button BtnBackSpace;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
+
---
+++ ProjeHesapMakinesi/FrmHesapMakinesi.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjeHesapMakinesi
+{
+    public partial class FrmHesapMakinesi : Form
+    {
+        int secim = 0;
+        double sayi1,sayi2,sonuc;
+
+
+        public FrmHesapMakinesi()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmHesapMakinesi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Butonlar(object sender, EventArgs e)
+        {
+            if (TxtDisplay.Text == "0")
+            {
+                TxtDisplay.Text = "";
+            }
+
+            TxtDisplay.Text = TxtDisplay.Text + ((Button)sender).Text;
+
+        }
+
+        private void BtnTopla_Click(object sender, EventArgs e)
+        {
+            sayi1 = double.Parse(TxtDisplay.Text);
+            secim = 1;
+            TxtDisplay.Text = "0";
+        }
+
+        private void BtnCıkar_Click(object sender, EventArgs e)
+        {
+            sayi1 = double.Parse(TxtDisplay.Text);
+            secim = 2;
+            TxtDisplay.Text = "0";
+        }
+
+        private void BtnCarp_Click(object sender, EventArgs e)
+        {
+            sayi1 = double.Parse(TxtDisplay.Text);
+            secim = 3;
+            TxtDisplay.Text = "0";
+        }
+
+        private void BtnBol_Click(object sender, EventArgs e)
+        {
+            sayi1 = double.Parse(TxtDisplay.Text);
+            secim = 4;
+            TxtDisplay.Text = "0";
+        }
+
+        private void BtnHesapla_Click(object sender, EventArgs e)
+        {
+            sayi2 = double.Parse(TxtDisplay.Text);
+
+            if (secim == 1)
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            else if (secim == 2)
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (secim == 3)
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else if (secim == 4)
+            {
+                // 0 a bölünme hatası
+                if (sayi2 != 0)
+                {
+                    sonuc = sayi1 / sayi2;
+                }
+                else
+                {
+                    MessageBox.Show("Sıfıra Bölünemez!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    TxtDisplay.Text = "0";
+                    return;
+                }
+            }
+
+            TxtDisplay.Text = sonuc.ToString();
+        }
+
+
+        private void BtnVirgul_Click(object sender, EventArgs e)
+        {
+            if (TxtDisplay.Text.IndexOf(",")<1)
+            {
+                TxtDisplay.Text += ",";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (TxtDisplay.Text != "0")
+            {
+                if (TxtDisplay.Text.StartsWith("-"))
+                {
+                    TxtDisplay.Text = TxtDisplay.Text.Replace("-", "");
+                }
+                else
+                {
+                    TxtDisplay.Text = "-" + TxtDisplay.Text;
+                }
+            }
+        }
+
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+            TxtDisplay.Text = "0";
+        }
+
+        private void BtnBackSpace_Click(object sender, EventArgs e)
+        {
+            TxtDisplay.Text = TxtDisplay.Text.Substring(0, TxtDisplay.Text.Length-1);
+           
+            if(TxtDisplay.Text == "")
+            {
+                TxtDisplay.Text = "0";
+            }
+        }
+    }
+}
